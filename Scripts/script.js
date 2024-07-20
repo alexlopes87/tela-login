0dab2cea6cd0b570ff572b0b6d8f1e0c863afa18
alexlopes87 <172157268+alexlopes87@users.noreply.github.com>
2024-07-20 15:30:35 -0300

    update-200724-1530

---
+++ Scripts/script.js
@@ -0,0 +1,14 @@
+function handleFocus(event) {
+    event.target.dataset.placeholder = event.target.placeholder;
+    event.target.placeholder = '';
+}
+
+function handleBlur(event) {
+    event.target.placeholder = event.target.dataset.placeholder;
+}
+
+document.getElementById('email').addEventListener('focus', handleFocus);
+document.getElementById('email').addEventListener('blur', handleBlur);
+
+document.getElementById('senha').addEventListener('focus', handleFocus);
+document.getElementById('senha').addEventListener('blur', handleBlur);
