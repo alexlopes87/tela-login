3529503e2c9ef72ff81d7cde5d0e05c98044925b
alexlopes87 <172157268+alexlopes87@users.noreply.github.com>
2024-07-22 03:30:48 -0300

    update melhorias no código

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,15 +1,17 @@
 /* Limpar placeholder nos campos input */
-function handleFocus(event) {
-    event.target.dataset.placeholder = event.target.placeholder;
-    event.target.placeholder = '';
-}
 
-function handleBlur(event) {
-    event.target.placeholder = event.target.dataset.placeholder;
-}
-
-document.getElementById('email').addEventListener('focus', handleFocus);
-document.getElementById('email').addEventListener('blur', handleBlur);
-
-document.getElementById('senha').addEventListener('focus', handleFocus);
-document.getElementById('senha').addEventListener('blur', handleBlur);
+function handleInputEvents(event) {
+    if (event.type === 'focus') {
+      event.target.dataset.placeholder = event.target.placeholder;
+      event.target.placeholder = '';
+    } else if (event.type === 'blur') {
+      event.target.placeholder = event.target.dataset.placeholder;
+    }
+  }
+  
+  const inputFields = document.querySelectorAll('.input-field');
+  
+  inputFields.forEach(input => {
+    input.addEventListener('focus', handleInputEvents);
+    input.addEventListener('blur', handleInputEvents);
+  });
